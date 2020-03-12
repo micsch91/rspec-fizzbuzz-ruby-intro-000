1deaf8b7c623fdbc0a7e807606d64efebb09237d
Michael Schneider <mic.sch91@gmail.com>
2020-03-12 00:37:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-    "FizzBuzz"
+    "Fizz"
   elsif int % 5 == 0
     "Buzz"
   elsif int % 3 && 5 == 0
-    "Fizz"
+    "FizzBuzz"
 end
   end
