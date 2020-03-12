8011eed3e92e224ca83f645b851b85f7fc4989d3
Michael Schneider <mic.sch91@gmail.com>
2020-03-12 00:15:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+end
+
+def fizzbuzz(fizz_5)
+  if fizz_5 % 5 == int
+    "Buzz"
+  end
+end
